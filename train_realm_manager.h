14dca6a34ab4f1ad0c267d73c1539e1bd07183f2
yanting0101 <timmychou950101@gmail.com>
2025-06-11 13:44:42 +0800

    Delete train_realm_manager.h

--- train_realm_manager.h
+++
@@ -1,9 +0,0 @@
-#ifndef TRAIN_REALM_MANAGER_H
-#define TRAIN_REALM_MANAGER_H
-
-#include "scene.h"
-#include <unordered_map>
-
-std::unordered_map<std::string, Scene> createTrainRealmScenes();
-
-#endif
